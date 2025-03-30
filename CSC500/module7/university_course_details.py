b51187c4fa79e256c022cec0154d6e14bf1dc1a9
SankaraNarayana.Katabathina <sankaranarayana.katabathina@t-mobile.com>
2025-03-29 23:30:45 -0500

    Module 7 critical thinking code changes

--- CSC500/module7/university_course_details.py
+++ CSC500/module7/university_course_details.py
@@ -1,24 +1,24 @@
 
 
-class Universiy_Cources:
-    cource_room = {}
-    cource_instructor = {}
-    cource_meeting = {}
-    cource_names = set()
+class Universiy_courses:
+    course_room = {}
+    course_instructor = {}
+    course_meeting = {}
+    course_names = set()
     def __init__(self):
-        self.load_cources_data()
+        self.load_courses_data()
 
-    def load_cources_data(self):
-        self.cource_room = {
+    def load_courses_data(self):
+        self.course_room = {
             "CSC101" : 3004,
             "CSC102": 4501,
             "CSC103": 6755,
             "NET110": 1244,
             "COM241": 1411
         }
-        self.cource_names.update(self.cource_room.keys())
+        self.course_names.update(self.course_room.keys())
 
-        self.cource_instructor = {
+        self.course_instructor = {
             "CSC101" : "Haynes",
             "CSC102": "Alvarado",
             "CSC103": "Rich",
@@ -26,8 +26,8 @@ class Universiy_Cources:
             "COM241": "Lee"
         }
 
-        self.cource_names.update(self.cource_instructor.keys())
-        self.cource_meeting = {
+        self.course_names.update(self.course_instructor.keys())
+        self.course_meeting = {
             "CSC101" : "8:00 a.m.",
             "CSC102": "9:00 a.m.",
             "CSC103": "10:00 a.m.",
@@ -35,29 +35,29 @@ class Universiy_Cources:
             "COM241": "1:00 p.m."
         }
         
-        self.cource_names.update(self.cource_meeting.keys())
+        self.course_names.update(self.course_meeting.keys())
 
 
 if __name__ == "__main__":
-    university = Universiy_Cources()
+    university = Universiy_courses()
     
-    university.load_cources_data()
-    print("University cource details are loaded")
+    university.load_courses_data()
+    print("University course details are loaded")
     
-    university_cources = " , ".join( e for e in university.cource_names)
-    print(f"Available Courses in the University: {university_cources}")
+    university_courses = " , ".join( e for e in university.course_names)
+    print(f"Available Courses in the University: {university_courses}")
     
     while True:
-        user_input = str(input("Enter the cource name (eg: CSC101, CSC102 ..), else 'q' to quit: ")).strip()
+        user_input = str(input("Enter the course name (eg: CSC101, CSC102 ..), else 'q' to quit: ")).strip()
         if user_input == "q":
             exit(1)
             
-        cource_names = (cource.strip() for cource in user_input.split(","))
+        course_names = (course.strip().upper() for course in user_input.split(","))
             
-        for cource_name in cource_names:
-            if (cource_name not in university.cource_names):
-                print(f"{cource_name} not available in the University")
+        for course_name in course_names:
+            if (course_name not in university.course_names):
+                print(f"\n{course_name} not available in the University")
             else:
-                print(f"\nCourceNamea: {cource_name}")
+                print(f"\ncourseName: {course_name}")
                 print("----------------")
-                print(f" RoomNumber: {university.cource_room.get(cource_name)}\n InstructorName: {university.cource_instructor.get(cource_name)}\n MeetinTime:{university.cource_meeting.get(cource_name)}")
+                print(f" RoomNumber: {university.course_room.get(course_name)}\n InstructorName: {university.course_instructor.get(course_name)}\n MeetingTime:{university.course_meeting.get(course_name)}")
